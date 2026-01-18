e17ea276c91f9b6fe34d94fa6d92016cfd11dc66
Nebi cetinkaya <nebic1972@gmail.com>
2026-01-18 22:18:51 +0300

    Fix: Configure webpack to ignore canvas for PDFjs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-/** @type {import('next').NextConfig} */
+//** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // --- YENİ EKLENEN KISIM (CANVAS HATASI İÇİN) ---
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
